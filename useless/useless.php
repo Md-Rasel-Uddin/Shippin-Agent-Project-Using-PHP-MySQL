c0fd7022c7a8b6230133f8c2b2c553331f6d27d4
Md Rasel Uddin <raseluddin102@gmail.com>
2023-09-12 19:50:06 +0600

    Add files via upload

---
+++ useless/useless.php
@@ -0,0 +1,17 @@
+value="<?php if(isset($_POST['first_name'])) echo $_POST['first_name']; ?>"
+<?php if(isset($_POST['last_name'])) echo $_POST['last_name']; ?>
+<?php if(isset($_POST['company_name'])) echo $_POST['company_name']; ?>
+<?php if(isset($_POST['licence_number'])) echo $_POST['licence_number']; ?>
+<?php if(isset($_POST['email_id'])) echo $_POST['email_id']; ?>
+<?php if(isset($_POST['type_of_company'])) echo $_POST['type_of_company']; ?>
+<?php if(isset($_POST['company_mail_id'])) echo $_POST['company_mail_id']; ?>
+<?php if(isset($_POST['country'])) echo $_POST['country']; ?>
+<?php if(isset($_POST['state'])) echo $_POST['state']; ?>
+<?php if(isset($_POST['userName'])) echo $_POST['userName']; ?>
+
+
+
+
+style="border-radius: 25px;"
+ style="border-radius: 25px;"
+ border-radius: 25px;
